a2b104770c1a0cd08526b907fff0197853829801
kwlayton1 <kwlayton1@yahoo.com>
2023-08-23 22:52:07 -0600

    adding_updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,13 +1,17 @@
 # Import the dependencies.
-
-
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import session
+from sqlalchemy import create_engine, inspect
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
 # reflect the tables
 
